2160a7223ad95e66de4c892b3b443a04eecc70d2
ubergesundheit <ubergesundheit@googlemail.com>
2017-12-11 19:25:43 +0100

    working WIP version

---
+++ src/client.js
@@ -0,0 +1,109 @@
+'use strict';
+
+const mqtt = require('mqtt'),
+  parseMQTTConfig = require('./mqttConfigParser'),
+  log = require('./logger');
+
+const RETRY_AFTER_MINUTES = 10,
+  NUMBER_RETRIES = 5;
+
+// use this object as simple key/value store for connecting/disconnecting
+const mqttConnections = {};
+
+const _retryAfter = function _retryAfter(box, afterMinutes) {
+  setTimeout(() => {
+    connect(box);
+  }, afterMinutes * 60000);
+};
+
+const getClient = function getClient(
+  { url, connectionOptions, topic },
+  maxRetries
+) {
+  let errRetries = maxRetries,
+    closeRetries = maxRetries;
+
+  const client = mqtt.connect(url, connectionOptions);
+  client.reconnecting = true;
+
+  return new Promise(function(resolve, reject) {
+    client.on('error', function(err) {
+      errRetries = errRetries - 1;
+      if (errRetries === 0) {
+        client.reconnecting = false;
+        client.end(true);
+
+        return reject(err);
+      }
+    });
+
+    client.on('close', function() {
+      closeRetries = closeRetries - 1;
+      if (closeRetries === 0) {
+        client.reconnecting = false;
+        client.end(true);
+
+        return reject(new Error(`connection closed after 5 retries.`));
+      }
+    });
+
+    client.on('connect', function() {
+      client.subscribe(topic, function(err) {
+        if (err) {
+          return reject(err);
+        }
+
+        return resolve(client);
+      });
+    });
+  });
+};
+
+const connect = async function connect(box) {
+  // disconnect any running connections before reconnecting..
+  disconnect(box);
+  let mqttCfg;
+  try {
+    mqttCfg = parseMQTTConfig(box);
+  } catch (err) {
+    log.error(err);
+  }
+
+  try {
+    const client = await getClient(
+      mqttCfg.internalConnectionOptions,
+      NUMBER_RETRIES
+    );
+    log.info(`connected mqtt for box ${box._id}`);
+
+    mqttConnections[box._id] = client;
+
+    client.on('close', function() {
+      log.info(`mqtt closed for box: ${box._id}`);
+    });
+
+    client.on('message', mqttCfg.decodeAndSaveMessage);
+  } catch (err) {
+    log.error(
+      err,
+      `mqtt error for box ${
+        box._id
+      }. Retry after ${RETRY_AFTER_MINUTES} minutes.`
+    );
+    // retry after..
+    _retryAfter(box, RETRY_AFTER_MINUTES);
+  }
+};
+
+const disconnect = function disconnect({ _id }) {
+  if (mqttConnections[_id]) {
+    mqttConnections[_id].end(true);
+    mqttConnections[_id] = undefined;
+    log.info(`mqtt disconnected mqtt for box ${_id}`);
+  }
+};
+
+module.exports = {
+  connect,
+  disconnect
+};
---
+++ src/mqttConfigParser.js
@@ -0,0 +1,135 @@
+'use strict';
+
+const log = require('./logger'),
+  { decoding, Box } = require('@sensebox/opensensemap-api-models');
+
+// see https://github.com/mqttjs/MQTT.js#client
+const ALLOWED_KEYS = [
+  'keepalive',
+  'reschedulePings',
+  'clientId',
+  'username',
+  'password'
+];
+
+// userConnectionOptions is a string which contains json
+const parseUserConnectionOptions = function parseUserConnectionOptions(
+  userConnectionOptions
+) {
+  const opts = {};
+
+  if (userConnectionOptions) {
+    const userOptions = JSON.parse(userConnectionOptions);
+
+    if (userOptions) {
+      // just handle keys in the ALLOWED_KEYS array
+      ALLOWED_KEYS.forEach(function(key) {
+        if (userOptions[key]) {
+          opts[key] = userOptions[key];
+        }
+      });
+    }
+  }
+
+  // check if there was a user supplied clientId
+  // and if not generate one..
+  if (!opts.clientId || typeof opts.clientId !== 'string') {
+    opts.clientId = `osem_${Math.random()
+      .toString(16)
+      .substr(2, 8)}`;
+  }
+
+  // check if there was a user supplied connectTimeout
+  // and if not set to 5 seconds
+  if (!opts.connectTimeout || typeof opts.connectTimeout !== 'string') {
+    opts.connectTimeout = 5 * 1000;
+  }
+
+  return opts;
+};
+
+/**
+ * Parse the mqtt integration configuration and return its validated properties
+ * @param {any} box a Box with mqtt integration configuration
+ * @return {Object} an object containing an internalConnectionOptions object
+ *          with url, topic, connectionOptions and a decodeAndSaveMessage
+ *          function along with the box id for the given input box
+ * @throws Invalid configuration will make this function throw an Error
+ */
+module.exports = function parseConfig(box) {
+  if (!box.integrations || !box.integrations.mqtt) {
+    throw new Error(`No mqtt configuration found for box ${box._id}`);
+  }
+
+  for (const prop of ['url', 'topic', 'messageFormat']) {
+    if (!box.integrations.mqtt[prop]) {
+      throw new Error(
+        `Missing mqtt configuration property ${prop} for box ${box._id}`
+      );
+    }
+  }
+
+  /* eslint-disable prefer-const */
+  let {
+    url,
+    topic,
+    messageFormat,
+    connectionOptions,
+    decodeOptions
+  } = box.integrations.mqtt;
+  /* eslint-enable prefer-const */
+
+  // validate connectionOptions
+  try {
+    connectionOptions = parseUserConnectionOptions(connectionOptions);
+  } catch (err) {
+    throw new Error(
+      `Connection options of box ${box._id} not parseable: ${err}`
+    );
+  }
+
+  // validate decodeOptions
+  try {
+    decodeOptions = JSON.parse(decodeOptions);
+  } catch (err) {
+    log.warn(`mqtt decode options of box ${box._id} not parseable: ${err}`);
+    decodeOptions = undefined;
+  }
+
+  // try to find a handler
+  const handler = decoding[messageFormat];
+  if (!handler) {
+    throw new Error(
+      `Invalid messageFormat "${messageFormat}" for box ${box._id}`
+    );
+  }
+
+  return {
+    _id: box._id,
+    internalConnectionOptions: {
+      url,
+      topic,
+      connectionOptions
+    },
+    async decodeAndSaveMessage(topic, message) {
+      // decode the measurements, query the database for the box,
+      // then save the measurements
+      try {
+        const decodedMeasurement = await handler.decodeMessage(
+          message.toString(),
+          decodeOptions
+        );
+        const theBox = await Box.findById(box._id);
+
+        const { ok, n } = theBox.saveMeasurementsArray(decodedMeasurement);
+        if (ok === n) {
+          log.info(
+            `received, decoded and saved mqtt message for box ${box._id}`
+          );
+        }
+      } catch (err) {
+        log.error(`error saving mqtt message for box ${box._id}: ${err}`);
+      }
+    }
+  };
+};
