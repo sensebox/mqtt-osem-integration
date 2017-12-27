19e504fc95dfcdf91506eb7fd7f5a77c50a329ab
ubergesundheit <ubergesundheit@googlemail.com>
2017-12-27 23:01:26 +0100

    add simple mqtt client test

--- test/test-mqtt.js
+++ test/test-mqtt.js
@@ -1,16 +1,45 @@
 'use strict';
 
-/* global describe it before after */
+/* global describe it before */
 const expect = require('chai').expect,
   dbConnectionString = require('./helpers/dbConnectionString'),
-  {
-    db: { connect, mongoose },
-    Box
-  } = require('@sensebox/opensensemap-api-models'),
-  senseBox = require('./helpers/senseBox');
+  { db: { connect }, Box } = require('@sensebox/opensensemap-api-models'),
+  senseBox = require('./helpers/senseBox'),
+  mqttClient = require('../src/client'),
+  mqtt = require('mqtt');
 
 describe('mqtt client', function() {
+  let testBox;
   before(async function() {
-    return connect(dbConnectionString({ db: 'mqttTest' }));
+    await connect(dbConnectionString({ db: 'mqttTest' }));
+
+    testBox = await Box.initNew(senseBox());
+    mqttClient.connect(testBox);
+    const mqclient = mqtt.connect(testBox.integrations.mqtt.url);
+
+    return new Promise(resolve => {
+      mqclient.on('connect', () => {
+        setTimeout(() => {
+          mqclient.subscribe('generalTestTopic');
+          mqclient.publish(
+            'generalTestTopic',
+            testBox.sensors.map(s => `${s._id},4`).join('\n')
+          );
+          setTimeout(() => {
+            resolve();
+          }, 500);
+        }, 500);
+      });
+    });
+  });
+
+  it('should accept measurements via mqtt message', async function() {
+    const box = await Box.findBoxById(testBox._id, {
+      onlyLastMeasurements: true
+    });
+
+    for (const sensor of box.sensors) {
+      expect(sensor.lastMeasurement.value).equal('4');
+    }
   });
 });
